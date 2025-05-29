6c99b651bae96101257d670fc4fb75682e51487d
David Pedersen <david.pdrsn@gmail.com>
2025-05-29 14:53:51 +0200

    misc changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -160,3 +160,14 @@ require('smart-splits').setup()
 require("keytrail").setup()
 
 vim.g.db_ui_use_nerd_fonts = 1
+
+require("oil").setup({
+    keymaps = {
+        ["<C-h>"] = false,
+    },
+    view_options = {
+        show_hidden = true,
+    }
+})
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
