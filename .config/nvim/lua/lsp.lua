ad2f4ba2c9c7d1b42ea579a57f49ddf31d363667
David Pedersen <david.pdrsn@gmail.com>
2025-06-04 00:04:34 +0200

    misc changes

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -149,7 +149,7 @@ require("blink.cmp").setup({
         },
     },
     completion = {
-        ghost_text = { enabled = true },
+        ghost_text = { enabled = false },
         documentation = {
             auto_show = true,
             auto_show_delay_ms = 500,
