7a18a67810597ef6724e896017a35279748e736c
David Pedersen <david.pdrsn@gmail.com>
2025-06-07 01:39:41 +0200

    misc changes

--- .config/nvim/lua/common.lua
+++ .config/nvim/lua/common.lua
@@ -48,4 +48,38 @@ function M.leader(mapping, what_to_do, options)
     M.nmap("<leader>" .. mapping, what_to_do, options)
 end
 
+function M.lsp_format_leader_command(pattern, augroup_name)
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup(augroup_name, {}),
+      pattern = pattern,
+      callback = function(ev)
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', '<space>lf', function()
+          vim.lsp.buf.format { async = true }
+        end, opts)
+      end,
+    })
+end
+
+function M.custom_format_leader_command(pattern, command, augroup_name)
+    vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup(augroup_name, {}),
+        pattern = pattern,
+        callback = function(ev)
+            local opts = { buffer = ev.buf }
+            local f = function()
+                vim.api.nvim_command('write')
+                local path = vim.api.nvim_buf_get_name(0)
+                -- vim.fn.system(command(path))
+                vim.fn.system { 'dotnet', 'csharpier', path }
+                local view = vim.fn.winsaveview()
+                vim.cmd('edit')
+                vim.fn.winrestview(view)
+            end
+            vim.keymap.set('n', '<space>lf', f, opts)
+        end,
+    })
+end
+
 return M
