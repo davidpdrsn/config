66caa60dea7bcf7006e14fec31df447a0d104da7
David Pedersen <david.pdrsn@gmail.com>
2025-06-12 00:41:56 +0200

    misc

--- home.nix
+++ home.nix
@@ -20,7 +20,7 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs.hello
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
@@ -35,14 +35,16 @@
     #   echo "Hello, ${config.home.username}!"
     # '')
   ];
+  
+  programs.git = {
+    enable = true;
+    includes = [{ path = "~/config/git/gitconfig"; }];
+  };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    ".gitignoreglobal".source = config/gitignoreglobal;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
