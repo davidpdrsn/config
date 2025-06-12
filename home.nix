5f8795ff7ac45ed2d8f55ebca2d490f392a27258
David Pedersen <davidpdrsn@Davids-MacBook-Pro.local>
2025-06-12 15:40:04 +0200

    misc

--- home.nix
+++ home.nix
@@ -1,117 +1,81 @@
 { config, pkgs, ... }:
 
 {
-    home.username = "davidpdrsn";
-    home.homeDirectory = "/Users/davidpdrsn";
+    # home.username = "davidpdrsn";
+    # home.homeDirectory = "/Users/davidpdrsn";
 
     # Don't change this value, even when updating home-manager.
     home.stateVersion = "25.05";
-
-    home.packages = with pkgs; [
-        htop
-        neovim
-        eza
-        bat
-        rustup
-        nerd-fonts.iosevka
-        atlas
-        jq
-        curl
-        wget
-        google-cloud-sdk
-
-        # lun
-        google-alloydb-auth-proxy
-
-        # typescript
-        prettierd
-
-        # go
-        gotools
-        golines
-        oapi-codegen
-        mockgen
-        golangci-lint
-        delve
-
-        # TODO
-        # postgresql
-        # nodejs_22, couldn't get `npm install` or `rush install` working
-        # nvm
-    ];
 
     # services.postgresql = {
     #     enable = true;
     # };
 
-    programs.zoxide.enable = true;
-    programs.fd.enable = true;
+    # programs.zoxide.enable = true;
+    # programs.fd.enable = true;
 
-    programs.git = {
-        enable = true;
-        includes = [{ path = "~/config/git/gitconfig"; }];
-    };
+    # programs.git = {
+    #     enable = true;
+    #     includes = [{ path = "~/config/git/gitconfig"; }];
+    # };
 
-    programs.zsh = {
-        enable = true;
-        initContent = builtins.readFile ./zsh/zshrc;
-        autosuggestion = {
-            enable = true;
-        };
-        syntaxHighlighting = {
-            enable = true;
-        };
-    };
+    # programs.zsh = {
+    #     enable = true;
+    #     initContent = builtins.readFile ./zsh/zshrc;
+    #     autosuggestion = {
+    #         enable = true;
+    #     };
+    #     syntaxHighlighting = {
+    #         enable = true;
+    #     };
+    # };
 
-    programs.fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        tmux = {
-            enableShellIntegration = true;
-        };
-    };
+    # programs.fzf = {
+    #     enable = true;
+    #     enableZshIntegration = true;
+    # };
 
-    programs.tmux = {
-        enable = true;
-        extraConfig = builtins.readFile ./tmux/tmux.conf;
-    };
+    # programs.tmux = {
+    #     enable = true;
+    #     extraConfig = builtins.readFile ./tmux/tmux.conf;
+    # };
 
-    programs.ripgrep = {
-        enable = true;
-        arguments = [
-            # Search hidden files / directories (e.g. dotfiles) by default
-            "--hidden"
-            # Search files in .gitignore
-            "--no-ignore"
-            # Using glob patterns to include/exclude files or folders
-            "--glob=!.git/*"
-            "--glob=!node_modules"
-            "--glob=!.godot/*"
-            "--glob=!build"
-            "--glob=!builds"
-            "--glob=!.cache"
-            "--glob=!temp"
-            "--glob=!*\.map"
-            "--glob=!target"
-            "--glob=!*\.log"
-            "--glob=!*\.DS_Store"
-            # Because who cares about case!?
-            "--smart-case"
-        ];
-    };
+    # programs.ripgrep = {
+    #     enable = true;
+    #     arguments = [
+    #         # Search hidden files / directories (e.g. dotfiles) by default
+    #         "--hidden"
+    #         # Search files in .gitignore
+    #         "--no-ignore"
+    #         # Using glob patterns to include/exclude files or folders
+    #         "--glob=!.git/*"
+    #         "--glob=!node_modules"
+    #         "--glob=!.godot/*"
+    #         "--glob=!build"
+    #         "--glob=!builds"
+    #         "--glob=!.cache"
+    #         "--glob=!temp"
+    #         "--glob=!*\.map"
+    #         "--glob=!target"
+    #         "--glob=!*\.log"
+    #         "--glob=!*\.DS_Store"
+    #         # Because who cares about case!?
+    #         "--smart-case"
+    #     ];
+    # };
 
-    programs.go = {
-        enable = true;
-        telemetry = {
-            mode = "off";
-        };
-    };
+    # programs.go = {
+    #     enable = true;
+    #     telemetry = {
+    #         mode = "off";
+    #     };
+    # };
 
-    home.file = {
-        ".gitignoreglobal".source = ./gitignoreglobal;
-        ".bin".source = ./bin;
-        ".config/ghostty".source = ./ghostty;
-    };
+    # home.file = {
+    #     ".gitignoreglobal".source = ./gitignoreglobal;
+    #     ".bin".source = ./bin;
+    #     ".config/ghostty".source = ./ghostty;
+    # };
 
     programs.home-manager.enable = true;
 }
