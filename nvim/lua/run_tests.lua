64f993ed7fdc6d86f0548c0e97141dc8839a5aee
David Pedersen <david.pdrsn@gmail.com>
2025-06-14 12:32:34 +0200

    Add tmux integration to run tests in existing tmux pane when available

--- nvim/lua/run_tests.lua
+++ nvim/lua/run_tests.lua
@@ -41,31 +41,67 @@ function run_test_command()
 
     vim.cmd("write")
 
-    local original_win_id = vim.api.nvim_get_current_win()
-    vim.cmd('botright 20new')
-    prev_test_buffer = vim.api.nvim_get_current_buf()
-    local job_id = vim.fn.jobstart(
-        test_command.command .. " " .. table.concat(test_command.args, " "),
-        {
-            term = true,
-            on_exit = function(_, status)
-                if status == 0 then
-                    vim.api.nvim_buf_delete(prev_test_buffer, { force = false })
-                    prev_test_buffer = nil
-                end
-            end,
-            on_stdout = function(_, data, _)
-                vim.schedule(function()
-                    -- Move cursor to last line
-                    local last_line = vim.api.nvim_buf_line_count(prev_test_buffer)
-                    vim.api.nvim_buf_call(prev_test_buffer, function()
-                        vim.api.nvim_win_set_cursor(0, { last_line, 0 })
+    local wrapped_cmd = tmux_wrap(test_command.command .. " " .. table.concat(test_command.args, " "))
+
+    if wrapped_cmd.in_tmux then
+        vim.fn.jobstart(wrapped_cmd.cmd)
+    else
+        local original_win_id = vim.api.nvim_get_current_win()
+        vim.cmd('botright 20new')
+        prev_test_buffer = vim.api.nvim_get_current_buf()
+        local job_id = vim.fn.jobstart(
+            wrapped_cmd.cmd,
+            {
+                term = true,
+                on_exit = function(_, status)
+                    if status == 0 then
+                        vim.api.nvim_buf_delete(prev_test_buffer, { force = false })
+                        prev_test_buffer = nil
+                    end
+                end,
+                on_stdout = function(_, data, _)
+                    vim.schedule(function()
+                        -- Move cursor to last line
+                        local last_line = vim.api.nvim_buf_line_count(prev_test_buffer)
+                        vim.api.nvim_buf_call(prev_test_buffer, function()
+                            vim.api.nvim_win_set_cursor(0, { last_line, 0 })
+                        end)
                     end)
-                end)
-            end,
-        }
-    )
-    vim.api.nvim_set_current_win(original_win_id)
+                end,
+            }
+        )
+        vim.api.nvim_set_current_win(original_win_id)
+    end
+end
+
+function is_not_in_tmux()
+    return os.getenv("TERM_PROGRAM") ~= "tmux"
+end
+
+function tmux_wrap(cmd)
+    if is_not_in_tmux() then
+        return { in_tmux = false, cmd = cmd }
+    end
+
+    local handle = io.popen("tmux list-panes -F '#{pane_index} #{pane_current_command}'", 'r')
+    local stdout = handle:read("*a")
+    handle:close()
+
+    for _, line in pairs(vim.split(stdout, '\n')) do
+        if line ~= "" then
+            local words = vim.split(line, ' ')
+            local n = words[1]
+            local pane_cmd = words[2]
+            if pane_cmd == "zsh" then
+                return {
+                    in_tmux = true,
+                    cmd = "tmux send-keys -t " .. n .. " \"" .. cmd .. "\" Enter",
+                }
+            end
+        end
+    end
+
+    return { in_tmux = false, cmd = cmd }
 end
 
 vim.keymap.set("n", "<leader>rr", function()
