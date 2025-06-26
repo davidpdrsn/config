fd804a4790c32119659c59644d2c857484068514
David Pedersen <david.pdrsn@gmail.com>
2025-06-26 16:47:02 +0200

    misc

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -16,7 +16,6 @@ return {
                 },
                 integrations = {
                     leap = true,
-                    gitsigns = true,
                 },
                 no_italic = true,
             })
@@ -149,33 +148,6 @@ return {
             vim.keymap.set("n", "<leader>u", ":UndotreeToggle<cr>", { desc = "Toggle undo tree" })
         end,
     },
-    -- git signs
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({
-                on_attach = function(bufnr)
-                    local gitsigns = require("gitsigns")
-
-                    vim.keymap.set("n", "]c", function()
-                        if vim.wo.diff then
-                            vim.cmd.normal({ "]c", bang = true })
-                        else
-                            gitsigns.nav_hunk("next")
-                        end
-                    end, { buffer = bufnr, desc = "Next hunk" })
-
-                    vim.keymap.set("n", "[c", function()
-                        if vim.wo.diff then
-                            vim.cmd.normal({ "[c", bang = true })
-                        else
-                            gitsigns.nav_hunk("prev")
-                        end
-                    end, { buffer = bufnr, desc = "Prev hunk" })
-                end,
-            })
-        end,
-    },
     -- godot
     { "habamax/vim-godot" },
     -- jump to matching thing
@@ -294,31 +266,10 @@ return {
         },
     },
     {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-    },
-    {
         "nvim-treesitter/nvim-treesitter-context",
         config = function()
             vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = false })
-
             require("treesitter-context").setup()
         end,
-        -- opts = {
-        --     -- enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-        --     -- multiwindow = false, -- Enable multiwindow support.
-        --     -- max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-        --     -- min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-        --     -- line_numbers = true,
-        --     -- multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        --     -- trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        --     -- mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-        --     -- -- Separator between context and content. Should be a single character string, like '-'.
-        --     -- -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-        --     -- separator = nil,
-        --     -- zindex = 20, -- The Z-index of the context window
-        --     -- on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-        -- },
     },
 }
