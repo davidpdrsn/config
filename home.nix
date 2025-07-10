d07db2e43f591fd04ac71536e9824d36b35a2d0a
David Pedersen <david.pdrsn@gmail.com>
2025-07-10 21:43:10 +0200

    Add starship config: disable newline, comment out format/character settings

--- home.nix
+++ home.nix
@@ -202,7 +202,18 @@ in {
     enable = true;
     enableNushellIntegration = true;
     settings = {
-      # ...
+      add_newline = false;
+      # format = lib.concatStrings [
+      #   "$line_break"
+      #   "$package"
+      #   "$line_break"
+      #   "$character"
+      # ];
+      # scan_timeout = 10;
+      # character = {
+      #   success_symbol = "➜";
+      #   error_symbol = "➜";
+      # };
     };
   };
 
