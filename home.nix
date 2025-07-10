1fa056880128ea6413b6812b0cc16554f39a00ec
David Pedersen <david.pdrsn@gmail.com>
2025-07-10 22:14:49 +0200

    Remove commented detect_files line from starship my_git_prompt config

--- home.nix
+++ home.nix
@@ -218,7 +218,6 @@ in {
       custom = {
         my_git_prompt = {
           command = "echo foo";
-          # detect_files = [];
           when = true;
           format = " transcending [$output]($style)";
         };
