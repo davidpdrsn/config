20793a9cd68447a2f471316a62c18966465c13b0
David Pedersen <david.pdrsn@gmail.com>
2025-07-10 22:51:41 +0200

    Remove some commented stuff

--- home.nix
+++ home.nix
@@ -238,10 +238,6 @@ in {
     enable = true;
     settings = {
       show_banner = false;
-      # completions.external = {
-      #   enable = true;
-      #   max_results = 200;
-      # };
     };
     shellAliases =
       {
