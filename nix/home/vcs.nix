96013f270dd22d5ea0e90faee330e4a5923d0ce3
David Pedersen <david.pdrsn@gmail.com>
2025-12-18 21:58:14 +0100

    default jj command to show ancestors of @

--- nix/home/vcs.nix
+++ nix/home/vcs.nix
@@ -75,7 +75,7 @@
         name = "David Pedersen";
       };
       ui = {
-        default-command = "log";
+        default-command = ["log" "-r" "::@" "--limit" "20"];
         pager = ":builtin";
       };
       colors = {
