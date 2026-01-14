f3884a9f2b8661a81f234dbd642587a9d1da6878
David Pedersen <david.pdrsn@gmail.com>
2026-01-14 21:01:37 +0100

    remove which-key

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -258,23 +258,4 @@ return {
     },
     -- notifications
     { "rcarriga/nvim-notify" },
-    -- which key
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    },
 }
