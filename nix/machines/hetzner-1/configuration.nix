759288a3d9668d655466894166070407151985eb
David Pedersen <david.pdrsn@gmail.com>
2026-03-06 17:03:33 +0100

    setup fyc.davidpdrsn.com

--- nix/machines/hetzner-1/configuration.nix
+++ nix/machines/hetzner-1/configuration.nix
@@ -33,6 +33,21 @@
           };
         };
       };
+      "fyc.davidpdrsn.com" = {
+        enableACME = true;
+        forceSSL = true;
+        locations = {
+          "/" = {
+            proxyPass = "http://127.0.0.1:3002";
+            extraConfig = ''
+              proxy_set_header Host $host;
+              proxy_set_header X-Real-IP $remote_addr;
+              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              proxy_set_header X-Forwarded-Proto $scheme;
+            '';
+          };
+        };
+      };
       "davidpdrsn.com" = {
         enableACME = true;
         forceSSL = true;
